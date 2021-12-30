a6c8e4fa63127ada57fae3c92ae2f22187017a22
Darkmose <pohomov1997@gmail.com>
2021-12-30 16:45:33 +0200

    Загрузил ДЗ по 10 заданию

---
+++ Lesson10/Assets/Scripts/MaterialChange.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour
+{
+    private const string ShaderBlendParamName = "_BlendScale";
+    private const float BlendMinValue = 0f;
+    private const float BlendMaxValue = 1f;
+    private Renderer _renderer;
+    [SerializeField] [Range(1f, 50f)]
+    private float _changeTextureSpeed = 1;
+    [SerializeField] [Range(0.001f, 0.3f)]
+    private float _changeTextureSteps = 0.01f;
+    private bool _isForwardAnimation = true;
+    private bool _isAnimated = false;
+
+    private void Awake()
+    {
+        if (TryGetComponent<Renderer>(out Renderer renderer))
+        {
+            _renderer = renderer;
+        }
+        else
+        {
+            Debug.Log($"Renderer on object [{this.gameObject.name}] doesn't exist!");
+        }
+    }
+
+    private IEnumerator ChangeTexture() 
+    {
+        if (!_isAnimated)
+        {
+            _isAnimated = true;
+            if (!_renderer)
+            {
+                yield return null;
+            }
+            else
+            {
+
+                if (_isForwardAnimation)
+                {
+                    for (float v = BlendMinValue; v <= BlendMaxValue; v += _changeTextureSteps)
+                    {
+                        _renderer.sharedMaterial.SetFloat(ShaderBlendParamName, v);
+                        yield return new WaitForSeconds(1 / _changeTextureSpeed);
+                    }
+                    _renderer.sharedMaterial.SetFloat(ShaderBlendParamName, 1);
+                    _isAnimated = false;
+                    _isForwardAnimation = false;
+                }
+                else
+                {
+                    for (float v = BlendMaxValue; v > BlendMinValue; v -= _changeTextureSteps)
+                    {
+                        _renderer.sharedMaterial.SetFloat(ShaderBlendParamName, v);
+                        yield return new WaitForSeconds(1 / _changeTextureSpeed);
+                    }
+                    _renderer.sharedMaterial.SetFloat(ShaderBlendParamName, 0);
+                    _isAnimated = false;
+                    _isForwardAnimation = true;
+                }
+            }
+        }        
+    }
+
+    private void OnMouseDown()
+    {
+        StartCoroutine(ChangeTexture());
+    }
+
+
+}
---
+++ Lesson10/Assets/Shaders/unlitShader.shader
@@ -0,0 +1,58 @@
+﻿Shader "Unlit/unlitShader"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Texture 1", 2D) = "white" {}
+        _SecondTex ("Texture 2", 2D) = "white" {}
+        _BlendScale("Blend Scale", Range(0, 1)) = 0.0
+    }
+        SubShader
+        {
+            Tags { "RenderType" = "Opaque" }
+            LOD 100
+
+            Pass
+            {
+                CGPROGRAM
+                #pragma vertex vert
+                #pragma fragment frag
+                #include "UnityCG.cginc"
+
+
+                struct appdata
+                {
+                    float4 vertex : POSITION;
+                    float2 uv : TEXCOORD0;
+                };
+
+                struct v2f
+                {
+                    float2 uv : TEXCOORD0;
+                    float4 vertex : SV_POSITION;
+                };
+
+                sampler2D _MainTex;
+                sampler2D _SecondTex;
+                half _BlendScale;
+                fixed4 _Color;
+
+                v2f vert (appdata v)
+                {
+                    v2f o;
+                    o.vertex = UnityObjectToClipPos(v.vertex);
+                    o.uv = v.uv;
+                    return o;
+                }
+
+                fixed4 frag (v2f i) : SV_Target
+                {
+                    fixed4 fragMainTex = tex2D(_MainTex, i.uv);
+                    fixed4 fragSecondTex = tex2D(_SecondTex, i.uv);
+                    fixed4 finalColor = lerp(fragMainTex, fragSecondTex, _BlendScale);
+                    return finalColor * _Color;
+                }
+                ENDCG
+            }
+        }
+}
